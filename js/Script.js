4aba5eb56e61dd2001a23beb540a621341e79637
anukalp <realanukalp@gmail.com>
2023-12-08 18:59:55 +0530

    commit

---
+++ js/Script.js
@@ -0,0 +1,34 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+var sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyM8UZ_KPwBjePzDLURbo6UWx8oYXdhwTnwx_zepHGYm5jrBqae8XEIGw1YtiN0F5sH/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+          setTimeout(function(){msg.innerHTML = ""
+        },500) 
+      form.reset()
+         })
+      .catch(error => console.error('Error!', error.message))
+  })
